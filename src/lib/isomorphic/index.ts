445d21eca780f4b7c3faff3d36aba76332890122
xiaolin1991 <113982716+xiaolin1991@users.noreply.github.com>
2023-10-11 10:45:42 +0800

    Delete src directory

--- src/lib/isomorphic/index.ts
+++
@@ -1,8 +0,0 @@
-'use client'
-
-import Debug from 'debug'
-export * from 'ifw'
-
-export const debug = typeof document === 'undefined' ? Debug('bingo')
-  : process.env.NEXT_PUBLIC_DEBUG ? console.info.bind(console)
-  : () => {}
--- src/pages/api/healthz.ts
+++
@@ -1,7 +0,0 @@
-'use server'
-
-import { NextApiRequest, NextApiResponse } from 'next'
-
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  res.status(200).end('ok')
-}
